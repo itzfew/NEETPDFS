449b46ebec7f710911e81562a8c1f9799ba58624
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 00:06:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,38 @@
-// pages/_document.js or components/CashfreeScript.js
+import '../styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 import Script from 'next/script';
-
-export const CashfreeScript = () => (
-  <Script
-    src="https://sdk.cashfree.com/js/v3/cashfree.js"
-    strategy="beforeInteractive"
-  />
-);
-
-// Then use it in your _app.js
-import { CashfreeScript } from '../components/CashfreeScript';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CashfreeScript />
+      <Head>
+        <title>EduCourse - Premium Learning Materials</title>
+        <meta name="description" content="Access premium study materials for competitive exams" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      {/* Load Cashfree SDK */}
+      <Script 
+        src="https://sdk.cashfree.com/js/v3/cashfree.js" 
+        strategy="beforeInteractive" 
+      />
+      
       <Component {...pageProps} />
-      <ToastContainer />
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
+
+export default MyApp;
