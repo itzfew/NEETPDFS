ce26f3decaa3c4fc24c9a5b05ae1d8dd6634267e
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 00:00:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+// next.config.js
 module.exports = {
   env: {
+    // Firebase config
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
@@ -8,7 +10,18 @@ module.exports = {
     NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    
+    // Cashfree config
     NEXT_PUBLIC_CASHFREE_APP_ID: process.env.NEXT_PUBLIC_CASHFREE_APP_ID,
     NEXT_PUBLIC_CASHFREE_ENV: process.env.NEXT_PUBLIC_CASHFREE_ENV,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/courses',
+        permanent: true,
+      },
+    ];
   },
 };
